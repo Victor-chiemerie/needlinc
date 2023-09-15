4b5d95e0ca3a0c7ebff373bdc046bf8068e573d5
Collins Ezedike-egwom <victorchiemerie02@gmail.com>
2023-09-15 08:44:54 +0100

    complete first signup page

--- lib/sign_inPage1_widget.dart
+++ lib/sign_inPage1_widget.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:needlinc/back_ground_widget.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class SignInPage1 extends StatefulWidget {
   const SignInPage1({super.key});
@@ -26,6 +27,24 @@ class _SignInPage1State extends State<SignInPage1> {
     ),
     borderRadius: BorderRadius.zero,
   );
+  bool isChecked = false;
+  void check(bool? newValue) {
+    setState(() {
+      isChecked = newValue ?? false;
+    });
+  }
+
+  Future<void> _launchGoogleHomePage() async {
+    final url = Uri(
+      scheme: 'https',
+      host: 'google.com',
+    );
+    if (await canLaunchUrl(url)) {
+      await launchUrl(url);
+    } else {
+      throw 'Could not launch $url';
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -35,8 +54,8 @@ class _SignInPage1State extends State<SignInPage1> {
           backGround_Widget(),
           Center(
             child: Container(
-              height: MediaQuery.of(context).size.height * 0.5,
-              width: MediaQuery.of(context).size.width * 0.8,
+              height: MediaQuery.of(context).size.height * 0.53,
+              width: MediaQuery.of(context).size.width * 0.9,
               margin: EdgeInsets.only(top: 60),
               decoration: BoxDecoration(
                   color: Colors.white,
@@ -50,62 +69,95 @@ class _SignInPage1State extends State<SignInPage1> {
                     )
                   ]),
               child: Padding(
-                padding: const EdgeInsets.all(15.0),
+                padding: const EdgeInsets.fromLTRB(15.0, 28, 15, 0),
                 child: Column(
                   children: [
-                    Text(
-                      'WELCOME TO NEEDLINC',
-                      style: TextStyle(
-                          fontSize: 20,
-                          fontWeight: FontWeight.w500,
-                          color: Colors.blue),
+                    // Welcome to NEEDLINC
+                    Row(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          'WELCOME TO',
+                          style: TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.w400,
+                          ),
+                        ),
+                        SizedBox(width: 7.0),
+                        Text(
+                          'NEEDLINC',
+                          style: TextStyle(
+                              fontSize: 25,
+                              fontWeight: FontWeight.w400,
+                              color: Colors.blue),
+                        ),
+                      ],
                     ),
                     Padding(
-                      padding: const EdgeInsets.fromLTRB(30, 10, 30, 10),
+                      padding: const EdgeInsets.fromLTRB(30, 15, 30, 0),
                       child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
                         children: [
+                          // Email or username
                           TextField(
                             decoration: InputDecoration(
                               hintText: 'Email or Username',
-                              contentPadding: EdgeInsets.symmetric(
-                                  vertical: 1.0, horizontal: 8.0),
+                              contentPadding:
+                                  EdgeInsets.symmetric(horizontal: 8.0),
                               focusedBorder: FocusedBorder,
                               enabledBorder: EnabledBorder,
                               errorBorder: ErrorBorder,
                             ),
                           ),
                           SizedBox(height: 10),
+                          // password
                           TextField(
                             decoration: InputDecoration(
                               hintText: 'Password',
-                              contentPadding: EdgeInsets.symmetric(
-                                  vertical: 1.0, horizontal: 8.0),
+                              contentPadding:
+                                  EdgeInsets.symmetric(horizontal: 8.0),
                               focusedBorder: FocusedBorder,
                               enabledBorder: EnabledBorder,
                               errorBorder: ErrorBorder,
                             ),
                           ),
-                          SizedBox(height: 10),
+                          SizedBox(height: 5),
+                          // Remember me
+                          Row(
+                            children: [
+                              Checkbox(
+                                value: isChecked,
+                                onChanged: check,
+                                visualDensity:
+                                    VisualDensity(horizontal: -4, vertical: -4),
+                              ),
+                              Text('Remember me'),
+                            ],
+                          ),
+                          SizedBox(height: 5),
+                          // Sign in
                           ElevatedButton(
                             onPressed: () {
-                              print('clicked');
+                              print('Signed in');
                             },
                             child: Text('Sign in'),
                             style: ElevatedButton.styleFrom(
-                              fixedSize: Size(190, 30),
+                              fixedSize: Size(230, 30),
                               shape: BeveledRectangleBorder(
                                 borderRadius: BorderRadius.zero,
                               ),
                             ),
                           ),
                           SizedBox(height: 10),
+                          // create new account
                           TextButton(
                             onPressed: () {
-                              print('clicked');
+                              print('created new account');
                             },
                             child: Text('Create New Account'),
                             style: TextButton.styleFrom(
-                              fixedSize: Size(190, 30),
+                              fixedSize: Size(230, 30),
                               shape: BeveledRectangleBorder(
                                 side: BorderSide(
                                   color: Colors.blue,
@@ -115,16 +167,43 @@ class _SignInPage1State extends State<SignInPage1> {
                             ),
                           ),
                           SizedBox(height: 10),
-                          TextButton(
-                            onPressed: () {
-                              print('clicked');
-                            },
+                          // forgot password?
+                          GestureDetector(
+                            onTap: _launchGoogleHomePage,
                             child: Text(
+                              'forgot password? Click here to reset',
+                              style: TextStyle(
+                                color: Colors.blue,
+                                fontSize: 12,
+                                decoration: TextDecoration.underline,
+                              ),
+                            ),
+                          ),
+                          SizedBox(height: 17),
+                          // or sign up as
+                          Row(
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: [
+                              Container(height: 1, width: 78, color: Colors.black,),
+                              SizedBox(width: 3),
+                              Text("or sign up as", style: TextStyle(fontSize: 10),),
+                              SizedBox(width: 3),
+                              Container(height: 1, width: 78, color: Colors.black,),
+                            ],
+                          ),
+                          SizedBox(height: 17),
+                          // Sign up with Google
+                          TextButton.icon(
+                            onPressed: () {
+                              print('Signed up with google');
+                            },
+                            icon: Icon(Icons.vpn_lock_rounded),
+                            label: Text(
                               'Sign up with Google',
                               style: TextStyle(color: Colors.black),
                             ),
                             style: TextButton.styleFrom(
-                              fixedSize: Size(190, 30),
+                              fixedSize: Size(230, 30),
                               shape: BeveledRectangleBorder(
                                 side: BorderSide(
                                   color: Colors.black,
@@ -134,16 +213,18 @@ class _SignInPage1State extends State<SignInPage1> {
                             ),
                           ),
                           SizedBox(height: 10),
-                          TextButton(
+                          // Sign up with Facebook
+                          TextButton.icon(
                             onPressed: () {
-                              print('clicked');
+                              print('Signed up with facebook');
                             },
-                            child: Text(
+                            icon: Icon(Icons.facebook),
+                            label: Text(
                               'Sign up with Facebook',
                               style: TextStyle(color: Colors.black),
                             ),
                             style: TextButton.styleFrom(
-                              fixedSize: Size(190, 30),
+                              fixedSize: Size(230, 30),
                               shape: BeveledRectangleBorder(
                                 side: BorderSide(
                                   color: Colors.black,
